0c2234fddf05093cdb4b0e0c1ce5a69470749a91
robisoncrisbrito <robison.brito@gmail.com>
2025-05-17 09:14:11 -0300

    com código de acesso ao GPS com LocationManager

--- app/src/main/java/br/edu/utfpr/usandohttp_pos2025/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandohttp_pos2025/MainActivity.kt
@@ -1,14 +1,62 @@
 package br.edu.utfpr.usandohttp_pos2025
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    private lateinit var tvLatitude: TextView
+    private lateinit var tvLongitude: TextView
+
+    private lateinit var locationManager: LocationManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvLatitude = findViewById(R.id.tvLatitude)
+        tvLongitude = findViewById(R.id.tvLongitude)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, this)
+
     }
+
+    fun btVerMapaOnClick(view: View) {
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+        tvLatitude.text = location.latitude.toString()
+        tvLongitude.text = location.longitude.toString()
+    }
+
 }
